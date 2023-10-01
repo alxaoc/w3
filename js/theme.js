25f757d10e50200fcf66e3d67811af9734c1b5e0
MoToR <alex.bublik.82@gmail.com>
2023-10-01 15:40:22 +0300

    change next css

--- js/theme.js
+++ js/theme.js
@@ -4,6 +4,20 @@ function toggleTheme() {
     body.classList.toggle('dark');
 }
 
+// Зміна теми
+// function toggleTheme() {
+//     var body = document.querySelector('body');
+//     body.classList.toggle('dark');
+//
+//     // Знайдіть всі елементи з класом "column-next"
+//     var columnNextElements = document.querySelectorAll('.column-next');
+//
+//     // Пройдіться по всіх елементах і додайте або видаліть клас "dark" відповідно до поточного стану
+//     columnNextElements.forEach(function(element) {
+//         element.classList.toggle('dark');
+//     });
+// }
+
 // Іконка
 // function toggleTheme() {
 //     var body = document.querySelector('body');
